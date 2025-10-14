7c15fbb1f759652b2118f8cf4cf74bd189deb5d4
Markus Ineichen <kusi.ineichen@gmail.com>
2025-10-14 17:15:04 +0200

    Initial commit using leptos with thaw-beta

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Deterministic Rust + WASM + Tailwind dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rust = with fenix.packages.${system}; combine [
+          stable.toolchain
+          targets.wasm32-unknown-unknown.stable.rust-std
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.trunk
+            pkgs.tailwindcss_4
+            pkgs.cargo-leptos
+            pkgs.sass
+          ];
+
+          shellHook = ''
+            echo "===================================="
+            echo " Welcome to the deterministic dev shell! "
+            echo "===================================="
+            rustc --version
+            cargo --version
+            trunk --version
+            tailwindcss --version
+          '';
+        };
+      });
+}

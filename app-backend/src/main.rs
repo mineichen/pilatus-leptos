efe263d63b9774d239bc646fc5bb80d2ebf3eb39
Markus Ineichen <kusi.ineichen@gmail.com>
2025-11-12 10:06:58 +0100

    Add app backend

---
+++ app-backend/src/main.rs
@@ -0,0 +1,31 @@
+use minfac::ServiceCollection;
+use pilatus::Name;
+use pilatus_rt::Runtime;
+
+fn main() {
+    Runtime::default()
+        .register(pilatus_axum_rt::register)
+        .register(pilatus_tick::register)
+        .register(register)
+        .run();
+}
+
+extern "C" fn register(c: &mut ServiceCollection) {
+    c.register(|| {
+        // Defines the default actor configuration
+        pilatus::InitRecipeListener::new(move |r| {
+            r.add_device(
+                pilatus_tick::create_default_timer_tick_device_config()
+                    .with_name(Name::new("Timer").unwrap()),
+            );
+            r.add_device(
+                pilatus_tick::create_default_manual_tick_device_config()
+                    .with_name(Name::new("Manual").unwrap()),
+            );
+            r.add_device(
+                pilatus_tick::create_default_greeter_device_config()
+                    .with_name(Name::new("Greeter").unwrap()),
+            );
+        })
+    });
+}
